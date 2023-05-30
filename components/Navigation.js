468e7e2a1e3eaa688d42731275828db2dd4f468b
surajsharma <surajsharma@pm.me>
2023-05-30 21:29:22 +0530

    avatar upload

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,18 +1,24 @@
-import { useCallback, useState } from 'react'
-import styles from "../styles/Navigation.module.css"
-import { Heart, Avatar, Cart } from './Icons'
+import { useCallback, useState } from "react";
+import styles from "../styles/Navigation.module.css";
+import { Heart, Cart } from "./Icons";
 
-import SvgIcon from './SvgIcon'
-import { Spacer } from '@geist-ui/core'
+import SvgIcon from "./SvgIcon";
+import { Spacer } from "@geist-ui/core";
+import { Badge } from "@geist-ui/core";
+
+import Avatar from "../avatar.svg";
 
 export default function Navigation() {
-    return <div className={styles.nav}>
-                <SvgIcon icon={Heart}/>
-                <Spacer h={1} />
+  return (
+    <div className={styles.nav}>
+      <>
+        <SvgIcon icon={Heart} b={10}></SvgIcon>
+        <Spacer h={1} />
+      </>
+      <img src={"http://s.cdpn.io/3/kiwi.svg"} />
+      <Spacer h={1} />
 
-                <SvgIcon icon={Avatar}/>
-                <Spacer h={1} />
-
-                <SvgIcon icon={Cart}/>
-            </div>
+      <SvgIcon icon={Cart} b={9} />
+    </div>
+  );
 }
--- components/SvgIcon.js
+++ components/SvgIcon.js
@@ -1,10 +1,49 @@
-import styles from "../styles/Navigation.module.css"
-
-export default function SvgIcon({icon}) {
-    return <div class={styles.icon}>
-        <svg width={icon.width} height={icon.height}  xmlns="http://www.w3.org/2000/svg" viewBox={icon.solid.a.viewBox}>
-        {icon.solid.path.map((p,i)=>{
-            return <g key={i} fill={p.fill}><path d={p.d}/></g>
-        })}</svg>
+import styles from "../styles/Navigation.module.css";
+import { Badge } from "@geist-ui/core";
+export default function SvgIcon({ icon, b }) {
+  return b > 0 ? (
+    <div className={styles.icon}>
+      <Badge
+        style={{
+          fontSize: "20px",
+          position: "absolute",
+          marginLeft: "50px",
+          marginTop: "-20px"
+        }}
+      >
+        {b}
+      </Badge>
+      <svg
+        width={icon.width}
+        height={icon.height}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={icon.solid.a.viewBox}
+      >
+        {icon.solid.path.map((p, i) => {
+          return (
+            <g key={i} fill={p.fill}>
+              <path d={p.d} />
+            </g>
+          );
+        })}
+      </svg>
     </div>
+  ) : (
+    <div className={styles.icon}>
+      <svg
+        width={icon.width}
+        height={icon.height}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={icon.solid.a.viewBox}
+      >
+        {icon.solid.path.map((p, i) => {
+          return (
+            <g key={i} fill={p.fill}>
+              <path d={p.d} />
+            </g>
+          );
+        })}
+      </svg>
+    </div>
+  );
 }
